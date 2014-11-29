c45722704f3078a2bf80cf36eabd37ecfa2a9639
Sara Subijana Gracia <sara.subijana@gmail.com>
2014-11-29 19:31:31 +0100

    Actualizado contenido del curriculum

--- app/scripts/controllers/cvController.js
+++ app/scripts/controllers/cvController.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('webApp')
-  .controller('CurriculumVitaeController', ['$scope', 'jsonSrv', function ($scope,jsonSrv) {	 
-    $scope.header = 'Profile';
-    $scope.header = '';
-    $scope.loadContent = function(contentName) {
+  .controller('CurriculumVitaeController', ['$scope','jsonSrv', function ($scope,jsonSrv) {	 
+    var loadJsonData = function(contentName) {
       jsonSrv.getJsonContent(contentName).then(function(response){
         $scope.header = response.header;
         $scope.cvContent = response.data;
       });
+    }
+
+    $scope.loadContent = function(contentName) {
+      loadJsonData(contentName);
     };
+
+    $scope.header = 'Profile';
+    $scope.header = '';
+    loadJsonData('profile')
+
+
   }]);
